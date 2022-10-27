134d08af960e52d164eda7f0b5ff76a433a8dc59
biaginifilippo <61706429+biaginifilippo@users.noreply.github.com>
2022-10-27 16:02:03 +0200

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -3,6 +3,11 @@ var axios = require("axios").default;
 async function sendMsg (data){
     var options = {
   method: 'POST',
+  /*INSERIRE LOCALHOST OPPURE INDIRIZZO IP SU CUI GIRA IL SERVER*/
+  /*                    ___                                     */         
+  /*                     |                                      */         
+  /*                     |                                      */         
+  /*                     v                                      */ 
   url: 'http://192.168.0.131:5000/api/v1/dosatore',
   headers: {'Content-Type': 'application/json'},
   data: data  //{testo: data.testo}
